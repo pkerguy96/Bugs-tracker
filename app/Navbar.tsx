e00816e9441c995fdd92d2231f5116131b20706e
pkerguy96 <aymenelkor@hotmail.com>
2024-07-28 00:24:42 +0100

    first push

---
+++ app/Navbar.tsx
@@ -0,0 +1,48 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { FaBug } from "react-icons/fa";
+import classNames from "classnames";
+const Navbar = () => {
+  const currentPath = usePathname(); //to get the active link
+  console.log(currentPath);
+
+  const links = [
+    {
+      label: "Dashboard",
+      href: "/",
+    },
+    {
+      label: "Issues",
+      href: "/issues",
+    },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <FaBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+              key={link.href}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
